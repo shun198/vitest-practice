0889552924d0046db3f0560e54cfc0c7786511e1
shun198 <shunhiroseluvmri@gmail.com>
2023-11-19 14:18:57 +0900

    👍 add concurrent tests!

--- basic-testing-frontend/src/hooks/hooks.test.js
+++ basic-testing-frontend/src/hooks/hooks.test.js
@@ -1,47 +1,53 @@
-import { it, expect } from 'vitest';
+import {
+  it,
+  expect,
+  beforeAll,
+  beforeEach,
+  afterAll,
+  afterEach,
+  describe,
+} from 'vitest';
 
 import { User } from './hooks';
 
-it('should update the email', () => {
-  const testEmail = 'test@test.com';
-  const newTestEmail = 'test2@test.com';
+const testEmail = 'test@test.com';
+let user = new User(testEmail);
 
-  const user = new User(testEmail);
-  user.updateEmail(newTestEmail);
-
-  expect(user.email).toBe(newTestEmail);
+// 全てのテストを実行する前に1度だけ実行
+beforeAll(() => {});
+// 各テストを実行する前に毎回実行
+beforeEach(() => {
+  user = new User(testEmail);
 });
+// 各テストを実行した後に毎回実行
+afterAll(() => {});
+// 全てのテストを実行した後に1度だけ実行
+afterEach(() => {});
 
-it('should have an email property', () => {
-  const testEmail = 'test@test.com';
+describe.concurrent('メールの更新', () => {
+  it('should update the email', () => {
+    const newTestEmail = 'test2@test.com';
+    user.updateEmail(newTestEmail);
+    expect(user.email).toBe(newTestEmail);
+  });
 
-  const user = new User(testEmail);
+  it('should have an email property', () => {
+    expect(user).toHaveProperty('email');
+  });
 
-  expect(user).toHaveProperty('email');
-});
+  it('should store the provided email value', () => {
+    expect(user.email).toBe(testEmail);
+  });
 
-it('should store the provided email value', () => {
-  const testEmail = 'test@test.com';
+  it('should clear the email', () => {
+    user.clearEmail();
 
-  const user = new User(testEmail);
+    expect(user.email).toBe('');
+  });
 
-  expect(user.email).toBe(testEmail);
-});
+  it('should still have an email property after clearing the email', () => {
+    user.clearEmail();
 
-it('should clear the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-  user.clearEmail();
-
-  expect(user.email).toBe('');
-});
-
-it('should still have an email property after clearing the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-  user.clearEmail();
-
-  expect(user).toHaveProperty('email');
+    expect(user).toHaveProperty('email');
+  });
 });
