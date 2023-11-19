c309fa1285a29786b6cc5109707bc52d85bfe684
shun198 <shunhiroseluvmri@gmail.com>
2023-11-19 10:12:35 +0900

    👍 数字以外が入ったときの異常系テストを追加

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -11,3 +11,9 @@ test('配列内の数字の合計を計算', () => {
   const result = add(numbers);
   expect(result).toBe(expectedResult);
 });
+
+test('配列内に数字以外が存在するときNaNを返す', () => {
+  const inputs = ['invalid', 1, 2];
+  const result = add(inputs);
+  expect(result).toBeNaN;
+});
